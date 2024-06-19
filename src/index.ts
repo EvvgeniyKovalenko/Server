691d58d254cda61df2702c613c3d6eae77b91132
Evgenii Kovalenko <kovalenko.evgenii89@mail.ru>
2024-06-19 15:37:51 +0300

    second commit

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,16 @@
 import express, {Request, Response} from 'express'
 
 const app = express()
-const port = 3000
+const port = process.env.PORT||3000
 
 
 
 app.get('/',(req:Request,res:Response)=>{
     let greeting = 'hi'
     res.send(greeting)
+})
+app.get('/courses', (req,res)=>{
+    res.send('welcome to pur university, choose course you like')
 })
 
 app.listen(port, () => {
